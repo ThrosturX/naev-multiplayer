36481a0933472882e0a20b252073c0bd4079b39a
not-pyroman <45698803+not-pyroman@users.noreply.github.com>
2024-11-18 16:27:56 -0600

    another fix
    disable is deprecated in favour of setDisable

--- scripts/equipopt/templates/multiplayer.lua
+++ scripts/equipopt/templates/multiplayer.lua
@@ -139,7 +139,7 @@ local mplayer_cores = {
          return c
       end,
 	["Kestrel"] = function () return {
-         choose_one{ "Unicorp PT-2200 Core System", "Milspec Orion 8601 Core System", "Milspec Thalos 9802 Core System", "Milspec Orion 9901 Core System" },
+         choose_one{ "Unicorp PT-1750 Core System", "Milspec Orion 8601 Core System", "Milspec Thalos 9802 Core System", "Milspec Orion 9901 Core System" },
          choose_one{ "Nexus Bolt 3000 Engine", "Krain Remige Engine", "Tricon Typhoon Engine", },
 		 choose_one{ "Unicorp D-48 Heavy Plating", "S&K Heavy Combat Plating" },
 
